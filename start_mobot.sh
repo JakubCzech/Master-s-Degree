f459aab23ef1209c6ec1a858eb586bd89f58604f
JakubCzech <czechjakub@icloud.com>
2023-08-25 14:09:10 +0200

    TEst mobot

--- start_mobot.sh
+++ start_mobot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker compose -f compose_dev.yaml rm
-docker compose -f compose_dev.yaml up -d controller navigation && docker compose logs -f  navigation controller
-docker compose -f compose_dev.yaml kill controller navigation 
+docker compose -f compose_dev.yaml rm -f
+docker compose -f compose_dev.yaml up -d controller navigation && docker compose logs -f  navigation controller |grep -e error -e warn -e fatal
+docker compose -f compose_dev.yaml kill 
 docker compose -f compose_dev.yaml rm -f
